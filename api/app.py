eba4ec4ad3afe63833ed76d065c73fb55d3260e2
ageonic <angeorge.dev@gmail.com>
2021-08-03 14:38:37 -0400

    Add flask-migrate as a dependency to handle database migrations

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 # initialize global extensions
 db = SQLAlchemy()
+migrate = Migrate()
 
 from config import config_map, default_config_name
 
@@ -15,6 +17,7 @@ def create_app(config_name=default_config_name):
 
     # initialize flask extensions with app
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # register flask blueprints
     from task.routes import bp
