34cb4879e66bd51be03bf1c03dd1e9e0a6aeacfc
ageonic <angeorge.dev@gmail.com>
2021-08-03 14:04:38 -0400

    Setup application factory pattern and create file to handle configuration options

--- api/app.py
+++ api/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-from task.routes import bp
 
-app = Flask(__name__)
-app.register_blueprint(bp, url_prefix="/api/task")
+# initialize global extensions
+db = SQLAlchemy()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+from config import config_map, default_config_name
+
+
+def create_app(config_name=default_config_name):
+    # initialize the flask app
+    app = Flask(__name__)
+    app.config.from_object(config_map[config_name])
+
+    # initialize flask extensions with app
+    db.init_app(app)
+
+    # register flask blueprints
+    from task.routes import bp
+
+    app.register_blueprint(bp, url_prefix="/api/task")
+
+    return app
---
+++ api/config.py
@@ -0,0 +1,47 @@
+import os
+
+# load environment variables
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# get absolute path to project directory
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    """
+    Base configuration options for the flask app.
+    See https://flask.palletsprojects.com/en/2.0.x/config/ for details on configuration handling.
+    """
+
+    TESTING = False
+    SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(16))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "PRODUCTION_DATABASE_URI", "sqlite:///" + os.path.join(basedir, "prod.db")
+    )
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "dev.db")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
+
+
+# simple dictionary to help refer to config classes with string names
+config_map = {
+    "production": ProductionConfig,
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+}
+
+# the config name that will be used as the default when initializing the app
+# set the DEFAULT_CONFIG_NAME environment variable to production for production environments
+default_config_name = os.getenv("DEFAULT_CONFIG_NAME", "development")
