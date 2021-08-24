06e02c2f8e20857c3737703e45c959a2bed1c0d7
ageonic <angeorge.dev@gmail.com>
2021-08-23 23:19:25 -0400

    Implement view and edit modes for the task creator component

--- src/components/TaskCreator.vue
+++ src/components/TaskCreator.vue
@@ -1,5 +1,8 @@
 <template>
-  <form id="create-task">
+  <section v-if="mode === 'view'">
+    <h1 class="text-xl font-extrabold">{{ form.name }}</h1>
+  </section>
+  <form v-if="mode === 'create' || mode === 'edit'" id="create-task">
     <div class="text-gray-700">
       <label class="block mb-1" for="form-taskname">Task Name</label>
       <input
@@ -15,11 +18,12 @@
           focus:shadow-outline
         "
         type="text"
-        v-model="name"
+        v-model="form.name"
         required
       />
     </div>
     <button
+      v-if="mode === 'create'"
       class="
         my-2
         px-4
@@ -33,39 +37,91 @@
     >
       Add
     </button>
+    <button
+      v-if="mode === 'edit'"
+      class="
+        my-2
+        px-4
+        py-2
+        bg-green-400
+        text-white
+        rounded-md
+        hover:bg-green-500
+      "
+      @click.prevent="updateTask"
+    >
+      Save
+    </button>
   </form>
 </template>
 
 <script>
 import axios from "axios";
+import { onBeforeMount, reactive } from "vue";
 
 export default {
   props: {
-    id: { type: Number },
-    name: { type: String },
-    complete: { type: Boolean },
-    parentId: { type: Number, default: 0 },
+    taskId: { type: Number },
     mode: {
       validator(value) {
-        return ["create"].includes(value);
+        return ["create", "view", "edit"].includes(value);
       },
       required: true,
     },
   },
   setup(props, { emit }) {
+    const form = reactive({
+      id: null,
+      name: null,
+      complete: false,
+      parent_id: null,
+    });
+
     const createTask = () => {
       axios
-        .post("/api/task", {
-          name: props.name,
-          complete: props.complete,
-          parent_id: props.parent_id,
-        })
+        .post("/api/task", form)
         .then((res) => emit("create"))
-        .catch((error) => console.log(error));
+        .catch((error) => console.log(error.message));
     };
 
+    const getTask = () => {
+      if (!props.taskId) {
+        console.error("no task id specified");
+        return;
+      }
+
+      axios
+        .get(`/api/task/${props.taskId}`)
+        .then((res) => {
+          for (let key of Object.keys(form)) {
+            form[key] = res.data[key];
+          }
+        })
+        .catch((error) => console.log(error.message));
+    };
+
+    const updateTask = () => {
+      if (!props.taskId) {
+        console.error("no task id specified");
+        return;
+      }
+
+      axios
+        .put(`/api/task/${props.taskId}`, form)
+        .then((res) => emit("update"))
+        .catch((error) => console.log(error.message));
+    };
+
+    onBeforeMount(() => {
+      // prefill existing task information if in view or edit mode
+      if (props.mode === "view" || props.mode === "edit") getTask();
+    });
+
     return {
+      form,
       createTask,
+      getTask,
+      updateTask,
     };
   },
 };
