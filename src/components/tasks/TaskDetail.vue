117ad8005a0c7abcb53789bc053a27105eee6798
ageonic <angeorge.dev@gmail.com>
2021-08-25 13:44:19 -0400

    Add basic component to display task details

---
+++ src/components/tasks/TaskDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>{{ task.name }}</h2>
+</template>
+
+<script>
+import { onMounted, reactive } from "@vue/runtime-core";
+import { getOneTask } from "../../services/TaskApi.js";
+
+export default {
+  props: {
+    taskId: { type: Number, required: true },
+  },
+  setup(props) {
+    const task = reactive({});
+
+    onMounted(() => {
+      getOneTask(props.taskId).then((t) => {
+        Object.assign(task, t);
+      });
+    });
+
+    return { task };
+  },
+};
+</script>
+
+<style>
+</style>
