c04869aad8abc6276a1bed9be7c7eb66140475e9
ageonic <angeorge.dev@gmail.com>
2021-08-27 10:54:29 -0400

    Add component to display a provided list of tasks

---
+++ src/components/tasks/TaskList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    v-for="task in tasks"
+    :key="task.id"
+    class="
+      my-2
+      cursor-pointer
+      focus:outline-none
+      focus:ring-2 focus:ring-indigo-200
+    "
+    role="button"
+    tabindex="0"
+    @click="handleClick(task.id)"
+    @keyup.enter="handleClick(task.id)"
+  >
+    <TaskListItem
+      :taskId="task.id"
+      :name="task.name"
+      status="in progress"
+      color="#FF8B72"
+    />
+  </div>
+</template>
+
+<script>
+import TaskListItem from "./TaskListItem.vue";
+
+export default {
+  components: { TaskListItem },
+  props: {
+    tasks: { type: Array, default: [] },
+  },
+  emits: ["select"],
+  setup(props, { emit }) {
+    const handleClick = (targetId) => {
+      emit("select", targetId);
+    };
+
+    return { handleClick };
+  },
+};
+</script>
+
+<style>
+</style>
