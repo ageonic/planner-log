09712a5fcde9f854478e4ad2df24be7699f0bc70
ageonic <angeorge.dev@gmail.com>
2021-08-27 15:24:54 -0400

    Allow editing of a task through the TaskDetail component

--- src/components/tasks/TaskDetail.vue
+++ src/components/tasks/TaskDetail.vue
@@ -2,14 +2,27 @@
   <article class="h-auto w-full shadow-lg bg-white rounded-md flex flex-col">
     <section
       name="header"
-      class="border-b-2 border-gray-100 flex items-center px-8 py-6"
+      class="border-b-2 border-gray-100 flex items-center px-8 py-4"
     >
       <div class="flex flex-grow items-center">
         <div class="mr-2"><StatusIndicator color="#7896FF" /></div>
         <div class="flex-grow capitalize text-gray-300">task status</div>
+        <div v-if="!editMode">
+          <button
+            @click="editMode = true"
+            class="
+              p-2
+              rounded-md
+              focus:outline-none
+              focus:ring-2 focus:ring-indigo-200
+            "
+          >
+            Edit task
+          </button>
+        </div>
       </div>
     </section>
-    <section name="details" class="p-8">
+    <section v-if="!editMode" name="details" class="p-8">
       <h1 class="text-2xl">{{ task.name }}</h1>
       <p class="mt-6 tracking-wider leading-6">
         Candy chocolate cake toffee powder ice cream cheesecake. Candy canes
@@ -17,6 +30,15 @@
         jelly-o donut dragée candy candy canes jujubes chupa chups. Jelly halvah
         candy canes marshmallow tart.
       </p>
+    </section>
+    <section v-if="editMode" name="task-editor" class="p-8">
+      <TaskEditor
+        :taskId="task.id"
+        :name="task.name"
+        :parentId="task.parent_id"
+        @update="handleUpdate"
+        @cancel="editMode = false"
+      />
     </section>
     <section
       name="subtasks"
@@ -29,17 +51,24 @@
 </template>
 
 <script>
-import { onMounted, reactive } from "@vue/runtime-core";
+import { onMounted, reactive, ref } from "@vue/runtime-core";
 import { getOneTask } from "../../services/TaskApi.js";
 import StatusIndicator from "../ui/StatusIndicator.vue";
+import TaskEditor from "./TaskEditor.vue";
 
 export default {
-  components: { StatusIndicator },
+  components: { StatusIndicator, TaskEditor },
   props: {
     taskId: { type: Number, required: true },
   },
   setup(props) {
     const task = reactive({});
+    const editMode = ref(false);
+
+    const handleUpdate = (updatedTask) => {
+      Object.assign(task, updatedTask);
+      editMode.value = false;
+    };
 
     onMounted(() => {
       getOneTask(props.taskId).then((t) => {
@@ -47,7 +76,7 @@ export default {
       });
     });
 
-    return { task };
+    return { task, editMode, handleUpdate };
   },
 };
 </script>
