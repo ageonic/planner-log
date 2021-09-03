edc7f2e255fb54cd1ea244ab3b67734b6f8f3c4c
ageonic <angeorge.dev@gmail.com>
2021-09-03 00:19:50 -0400

    Require token based authentication to access all task routes and deny access to tasks not owned by the logged in user

--- api/task/routes.py
+++ api/task/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, abort, request
+from flask import g, Blueprint, abort, request
 from flask_restful import Api, Resource, fields, marshal_with
+from auth.helper import token_required
 
 from models import db, Task
 
@@ -23,17 +24,29 @@ task_tree_serializer = {k: v for k, v in task_serializer.items()}
 task_tree_serializer["subtasks"] = fields.Nested(task_tree_serializer)
 
 
+def get_task_or_403(task_id):
+    """Retrieve a specified task and abort if the task does not belong to the logged in user."""
+    task = Task.query.get_or_404(task_id)
+
+    if not task.owned_by_user(g.user.id):
+        abort(403, "insufficient access to the requested task")
+
+    return task
+
+
 class TaskEntry(Resource):
     """Retrieve, update, or delete a single task entry."""
 
+    @token_required
     @marshal_with(task_serializer)
     def get(self, id):
-        return Task.query.get_or_404(id)
+        return get_task_or_403(id)
 
+    @token_required
     @marshal_with(task_serializer)
     def put(self, id):
         data = request.get_json()
-        task = Task.query.get_or_404(id)
+        task = get_task_or_403(id)
 
         for k, v in data.items():
             setattr(task, k, v)
@@ -42,8 +55,10 @@ class TaskEntry(Resource):
         db.session.commit()
         return task
 
+    @token_required
     def delete(self, id):
-        task = Task.query.get_or_404(id)
+        task = get_task_or_403(id)
+
         db.session.delete(task)
         db.session.commit()
         return "", 201
@@ -52,24 +67,28 @@ class TaskEntry(Resource):
 class TaskTree(Resource):
     """Retrieve a task along with all descendant tasks."""
 
+    @token_required
     @marshal_with(task_tree_serializer)
     def get(self, id):
-        return Task.query.get_or_404(id)
+        return get_task_or_403(id)
 
 
 class TaskList(Resource):
-    """Retrieve a list of all tasks or create a single task."""
+    """Retrieve a list of all tasks or create a single task for the current user."""
 
+    @token_required
     @marshal_with(task_serializer)
     def get(self):
-        return Task.query.all()
+        return Task.query.filter_by(user_id=g.user.id).all()
 
+    @token_required
     @marshal_with(task_serializer)
     def post(self):
         data = request.get_json()
         task = Task(
             name=data.get("name"),
             parent_id=data.get("parent_id"),
+            user_id=g.user.id,
         )
         db.session.add(task)
         db.session.commit()
