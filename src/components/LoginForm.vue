1e132a3ee094995142c4ab50fef37d40668dd3ef
ageonic <angeorge.dev@gmail.com>
2021-09-03 13:03:28 -0400

    Add LoginForm component to accept basic user credentials

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <form @submit.prevent="submit">
+    <div class="my-4">
+      <TextInput
+        label="username"
+        name="username"
+        :required="true"
+        v-model="username"
+      />
+    </div>
+    <div class="my-4">
+      <TextInput
+        label="password"
+        name="password"
+        type="password"
+        :required="true"
+        v-model="password"
+      />
+    </div>
+    <div class="my-4 w-full text-center">
+      <button
+        type="submit"
+        class="py-2 w-full rounded bg-indigo-500 text-white hover:bg-indigo-600"
+      >
+        Login
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import TextInput from "./ui/inputs/TextInput.vue";
+
+export default {
+  components: { TextInput },
+  emits: ["submit"],
+  setup(props, { emit }) {
+    const username = ref("");
+    const password = ref("");
+
+    const submit = () => {
+      emit("submit", { username: username.value, password: password.value });
+    };
+
+    return { username, password, submit };
+  },
+};
+</script>
+
+<style>
+</style>
