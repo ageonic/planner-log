df28f9b30e6a9c4ffc8a105f4fcc51e448876c23
ageonic <angeorge.dev@gmail.com>
2021-09-04 19:30:20 -0400

    Add a new component to represent the login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full flex items-center justify-center">
+    <section class="p-8 w-96 bg-white">
+      <LoginForm @submit="submit" />
+    </section>
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm.vue";
+import UserAuth from "../services/UserAuth";
+
+export default {
+  components: { LoginForm },
+  setup() {
+    const submit = (auth_data) => {
+      UserAuth.login(auth_data);
+    };
+
+    return { submit };
+  },
+};
+</script>
+
+<style>
+</style>
