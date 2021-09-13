315bafae447bc05d3a043cecfc85553c7be5f6aa
ageonic <angeorge.dev@gmail.com>
2021-09-13 17:14:22 -0400

    Add route to view daily planner entries

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import LoginPage from "./pages/LoginPage.vue";
+import DailyPage from "./pages/DailyPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/login", component: LoginPage },
+  { path: "/daily/:id?", component: DailyPage },
 ];
 
 export const router = createRouter({
