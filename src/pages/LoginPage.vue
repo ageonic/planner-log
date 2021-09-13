d104f1aec0cf1c75743621d80c950e6b46f70be3
ageonic <angeorge.dev@gmail.com>
2021-09-13 10:09:08 -0400

    Store task list for current user after log in is complete

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -10,12 +10,25 @@
 import LoginForm from "../components/LoginForm.vue";
 import UserAuth from "../services/UserAuth";
 import { router } from "../router";
+import { getTaskStatusList } from "../services/TaskApi";
+import UserStore from "../store/User";
+import TaskStore from "../store/Task";
 
 export default {
   components: { LoginForm },
   setup() {
     const submit = (auth_data) => {
-      UserAuth.login(auth_data).then(() => router.push("/"));
+      UserAuth.login(auth_data).then(() => {
+        // first check whether the auth token was successfully stored
+        if (UserStore.getters.authToken())
+          // store the list of task statuses for the current user
+          // as this is a resource that does not change very often
+          getTaskStatusList().then((statusList) =>
+            TaskStore.methods.setStatusList(statusList)
+          );
+
+        router.push("/");
+      });
     };
 
     return { submit };
