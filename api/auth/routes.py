77e3d6d09dbab7b1490ec71df28b80ea1913e35c
ageonic <angeorge.dev@gmail.com>
2021-09-15 11:18:25 -0400

    Add cli command to create a new user

--- api/auth/routes.py
+++ api/auth/routes.py
@@ -1,10 +1,29 @@
+import click
 from flask import Blueprint, request, current_app
 from flask_restful import Api, Resource
-from models import User
+from models import db, User
 
 # initialize blueprint
 bp = Blueprint("auth", __name__)
 api = Api(bp)
+
+
+@bp.cli.command("create")
+@click.option("-u", "--username", "username", required=True)
+@click.option("-p", "--password", "password", required=True)
+def create_user(username, password):
+    """Create a user"""
+    if User.query.filter_by(username=username).first() is None:
+        user = User(username, password)
+        db.session.add(user)
+        db.session.commit()
+        click.echo(
+            "\nsuccessfully added user\n\nid: {}\nusername: {}".format(
+                user.id, user.username
+            )
+        )
+    else:
+        click.echo("\nusername '{}' already exists".format(username))
 
 
 class UserLogin(Resource):
