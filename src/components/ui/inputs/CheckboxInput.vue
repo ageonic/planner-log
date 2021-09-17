9455eaefb49b1eb750364f72a84ebdcebb052992
ageonic <angeorge.dev@gmail.com>
2021-09-17 00:15:20 -0400

    Add CheckboxInput component to simplify checkbox field creation in forms

---
+++ src/components/ui/inputs/CheckboxInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <label :for="name" class="block text-sm font-medium text-gray-500 capitalize">
+    {{ label }}
+  </label>
+  <input
+    :name="name"
+    :id="name"
+    type="checkbox"
+    :checked="modelValue"
+    @change="$emit('update:modelValue', $event.target.checked)"
+    class="
+      mt-1
+      p-2
+      block
+      shadow-sm
+      border border-gray-200
+      rounded-md
+      focus:outline-none
+      focus:ring-2 focus:ring-indigo-200
+    "
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
