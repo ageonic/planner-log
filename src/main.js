b23b3d44ee92dc9727828f707735da343a43e3f6
ageonic <angeorge.dev@gmail.com>
2021-09-17 18:01:37 -0400

    Add console.log to global properties for the app

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import UserStore from "./store/User";
 
 const app = createApp(App);
 
+app.config.globalProperties.$log = console.log;
+
 app.use(router);
 app.provide("user", UserStore);
 app.mount("#app");
