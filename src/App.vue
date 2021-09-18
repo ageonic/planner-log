af0e1bccabc38657626f60ef4d7a455fef0c6f9f
ageonic <angeorge.dev@gmail.com>
2021-09-17 22:54:39 -0400

    Add simple side navigation component that only appears when logged in

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="container mx-auto">
-    <router-view />
+  <div class="flex flex-col h-screen">
+    <div class="flex flex-1 overflow-hidden">
+      <aside v-if="user.getters.authToken()" class="flex w-64">
+        <TheSidenav />
+      </aside>
+      <main class="flex flex-1 flex-col shadow-lg">
+        <div class="flex-1 overflow-y-auto p-8">
+          <router-view />
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -8,12 +17,17 @@
 import { inject } from "@vue/runtime-core";
 import { router } from "./router";
 
+import TheSidenav from "./components/TheSidenav.vue";
+
 export default {
+  components: { TheSidenav },
   setup() {
     const user = inject("user");
 
     // redirect the user to the login page if the auth token is not set
     if (!user.getters.authToken()) router.push("/login");
+
+    return { user };
   },
 };
 </script>
@@ -24,6 +38,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheSidenav.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="h-full w-full bg-gray-200">
+    <ul class="">
+      <li class="hover:bg-gray-50">
+        <router-link class="px-6 py-2 block" to="/">Home</router-link>
+      </li>
+      <li class="hover:bg-gray-50">
+        <router-link class="px-6 py-2 block" to="/daily">Today</router-link>
+      </li>
+      <li class="hover:bg-gray-50">
+        <router-link class="px-6 py-2 block" to="/settings">
+          Settings
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
